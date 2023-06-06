53234997b71119d0615f5ca3cdf1cd093ada75fd
vamsi113 <krishultimate5@gmail.com>
2023-06-06 18:30:45 -0400

    change

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "rabbitmq" {
-  ami = data.aws_ami.centos-8-ami.id
+  ami = data.aws_ami.centos-8-ami.image_id
   instance_type = var.instance_type
   subnet_id = var.subnets[0]
   tags = {
