b4bfbdc5a2da80b08c0a4c3751fcc64bc6b42f64
vamsi113 <krishultimate5@gmail.com>
2023-06-19 14:20:16 -0400

    change

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "rabbitmq" {
   zone_id = var.private_zone_id
   name    = "rabbitmq-${var.env}"
-  type    = "A"
+  type    = "CNAME"
   ttl     = 300
   records = [aws_instance.rabbitmq.private_ip]
 }
